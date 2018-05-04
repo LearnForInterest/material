78a5a2a13819a064da8d4c4db74b2a019471dc0a
gyq <u74315@yungtay.com.cn>
2018-05-04 15:39:29 +0800

    doctrine demo1 : create data in table channel

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,58 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends MY_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+
+        /** @var  $userGroup Entity\UserGroup */
+        $userGroup = $this->em->getRepository('Entity\UserGroup')->find(1);
+
+        $name  = '111';
+        $email = '111@lattecake.com';
+
+        /** @var  $userInfo Entity\User */
+        $userInfo = $this->em->getRepository('Entity\User')->findOneBy([
+            'username' => $name
+        ]);
+
+        if( $userInfo )
+        {
+            throw Doctrine\DBAL\Exception\NotNullConstraintViolationException::unknownColumnType("{$name} 已存在");
+        }
+
+        $user = new Entity\User;
+        $user->setUsername($name);
+        $user->setPassword($name);
+        $user->setEmail($email);
+        $user->setGroup($userGroup);
+
+        // $this->em->persist($user);
+        // $this->em->flush();
+        $this->twig->render('welcome_message.html', [
+            // 'user'   	=> $user,
+            // 'userGroup' => $userGroup
+        ]);
+    }
 }
